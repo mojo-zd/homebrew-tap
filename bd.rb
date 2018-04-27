411268337065314c39562f236d4f7c7c68d14a8f
mojo <excessivespeed@126.com>
2018-04-27 09:42:56 +0800

    update sha256

--- bd.rb
+++ bd.rb
@@ -3,7 +3,7 @@ class Bd < Formula
   homepage "https://github.com/mojo-zd/bd"
   url "https://github.com/mojo-zd/bd/releases/download/v0.0.1-beta/bddict_darwin_amd64-0.0.1-beta.tar.gz"
   version "v0.0.1-beta"
-  sha256 "2d010812157594d7bce45483a114d1990757fa3d5d6caf1decd20b51f34659e9"
+  sha256 "2b73e80a51fdaf982d3bc0f2e91896996c0547392f2d49e36380fc35bcb7cd10"
 
   def install
     bin.install "bd"
